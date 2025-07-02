d1ba8c459a1bbdb7f4c271ad8b2cd38037b2d851
gaurav002 <gp3062614@gmail.com>
2025-07-02 19:34:02 +0530

    adding react through CDN link

---
+++ Day1/app.js
@@ -0,0 +1,10 @@
+ const h2 = React.createElement('h1', '{}' , 'Hey naste react');
+         const getId1 = document.getElementById('root');
+        const root = ReactDOM.createRoot(getId1);
+        root.render(h2)
+
+
+        // const h1 = document.createElement('h1')
+        // h1.innerHTML='hey this is my code';
+        // const getId = document.getElementById('root');
+        // getId.appendChild(h1);
