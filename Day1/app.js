e45a7fa84564ef4bb68260fa1505bff47a557b1e
gauravP002 <gp3062614@gmail.com>
2025-07-05 11:33:11 +0530

    jsx and react dom

--- Day1/app.js
+++ Day1/app.js
@@ -1,7 +1,13 @@
- const h2 = React.createElement('h1', '{}' , 'Hey naste react');
-         const getId1 = document.getElementById('root');
-        const root = ReactDOM.createRoot(getId1);
-        root.render(h2)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// const h2 = React.createElement('h1', '{}' , 'Hey naste react');
+//          const getId1 = document.getElementById('root');
+//         const root = ReactDOM.createRoot(getId1);
+//         root.render(h2)
+
+        const h1 = <h1> Hey naste react </h1>;
+        const root = ReactDOM.createRoot(document.getElementById('root'));
+        root.render(h1);
 
 
-      
